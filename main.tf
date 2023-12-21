1071aeada13624905853a1219cfbfa1160916108
Faisal Akhtar <faisalakhtar12@gmail.com>
2023-12-21 13:26:46 -0500

    First commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+    backend "s3" {
+    bucket = "faisalstatebucket"
+    key    = "statefile"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
