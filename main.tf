5c20ef524090da1e94f4167150a6db2b6d6d75a8
Faisal Akhtar <faisalakhtar12@gmail.com>
2023-12-21 13:43:04 -0500

    testing with repo secrest

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
