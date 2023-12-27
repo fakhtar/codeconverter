b4c6c2bf216c5e815b30a2cff57fa5cf7105cede
Faisal Akhtar <faisalakhtar12@gmail.com>
2023-12-27 11:51:00 -0500

    Corrected lambda handler path and including requirements again

--- apilambda/lambda_function.py
+++ apilambda/lambda_function.py
@@ -1,24 +1,19 @@
-# import os
-# import os.path
-# import sys
+import os
+import os.path
+import sys
  
-# envLambdaTaskRoot = os.environ["LAMBDA_TASK_ROOT"]
-# print("LAMBDA_TASK_ROOT env var:"+os.environ["LAMBDA_TASK_ROOT"])
-# print("sys.path:"+str(sys.path))
+envLambdaTaskRoot = os.environ["LAMBDA_TASK_ROOT"]
+print("LAMBDA_TASK_ROOT env var:"+os.environ["LAMBDA_TASK_ROOT"])
+print("sys.path:"+str(sys.path))
  
-# sys.path.insert(0,envLambdaTaskRoot+"/NewBotoVersion")
-# print("sys.path:"+str(sys.path))
-# import botocore
-# import boto3
- 
-# print("boto3 version:"+boto3.__version__)
-# print("botocore version:"+botocore.__version__)
-
- 
-# def lambda_handler(event,context):
-#     print(event)
-import json
+sys.path.insert(0,envLambdaTaskRoot+"/NewBotoVersion")
+print("sys.path:"+str(sys.path))
+import botocore
 import boto3
+ 
+print("boto3 version:"+boto3.__version__)
+print("botocore version:"+botocore.__version__)
 
-def lambda_handler(event, context):
+ 
+def lambda_handler(event,context):
     print(event)
