9063d5e26eb3f039fee5a496bfe43b90e9bf488e
Faisal Akhtar <faisalakhtar12@gmail.com>
2023-12-27 12:41:31 -0500

    Ran terraform validate and format locally

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-    backend "s3" {
+  backend "s3" {
     bucket = "faisalstatebucket"
     key    = "statefile"
     region = "us-east-1"
--- resources.tf
+++ resources.tf
@@ -1,51 +1,51 @@
 
 # lambda functions that will call the bedrock runtime library
 resource "aws_lambda_function" "bedrocklambda" {
-    filename      = "lambda.zip"
-    function_name = "bedrocklambda"
-    role          = aws_iam_role.bedrocklambda_role.arn
-    handler       = "lambda_function.lambda_handler"
-    source_code_hash = filebase64sha256("lambda.zip")
-    runtime = "python3.8"
-    layers = []
+  filename         = "lambda.zip"
+  function_name    = "bedrocklambda"
+  role             = aws_iam_role.bedrocklambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = filebase64sha256("lambda.zip")
+  runtime          = "python3.8"
+  layers           = []
 }
 
 resource "aws_iam_role" "bedrocklambda_role" {
-    name = "bedrocklambda-role-githubactions"
-    inline_policy {
-        name = "my_inline_policy"
+  name = "bedrocklambda-role-githubactions"
+  inline_policy {
+    name = "my_inline_policy"
 
-        policy = jsonencode(
-        {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Sid": "VisualEditor0",
-                    "Effect": "Allow",
-                    "Action": [
-                        "bedrock:InvokeAgent",
-                        "bedrock:InvokeModel",
-                        "bedrock:InvokeModelWithResponseStream"
-                    ],
-                    "Resource": "arn:aws:bedrock:*::foundation-model/*"
-                }
-            ]
-        }
-        )
-    }
-    assume_role_policy = <<EOF
+    policy = jsonencode(
+      {
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            "Sid" : "VisualEditor0",
+            "Effect" : "Allow",
+            "Action" : [
+              "bedrock:InvokeAgent",
+              "bedrock:InvokeModel",
+              "bedrock:InvokeModelWithResponseStream"
+            ],
+            "Resource" : "arn:aws:bedrock:*::foundation-model/*"
+          }
+        ]
+      }
+    )
+  }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-        }
-    ]
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
-    EOF
+  ]
+}
+EOF
 }
