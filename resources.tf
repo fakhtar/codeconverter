0417486e54b611b3be0058e8f287c14ac8b01c9a
Faisal Akhtar <faisalakhtar12@gmail.com>
2023-12-29 12:56:00 -0500

    Increatd lambda timeout to 15 secs

--- resources.tf
+++ resources.tf
@@ -8,6 +8,7 @@ resource "aws_lambda_function" "bedrocklambda" {
   source_code_hash = filebase64sha256("lambda.zip")
   runtime          = "python3.8"
   layers           = []
+  timeout = 15
 }
 
 resource "aws_iam_role" "bedrocklambda_role" {
