b3a99f677dacecd08d49179495a371e52076ff8f
Faisal Akhtar <faisalakhtar12@gmail.com>
2024-01-06 08:05:30 -0500

    print response body

--- apilambda/lambda_function.py
+++ apilambda/lambda_function.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
        contentType='application/json'
      )  
     response_body = json.loads(response.get('body').read())
-    
+    print(response_body)
      # The response from the model now mapped to the answer
     answer = response_body.get('completions')[0].get('data').get('text')
      
