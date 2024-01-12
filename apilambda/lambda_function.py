44ffc49903a1376e563434a5c8516b11d41501da
Faisal Akhtar <faisalakhtar12@gmail.com>
2024-01-12 08:15:13 -0500

    Cleaned up lambda code

--- apilambda/lambda_function.py
+++ apilambda/lambda_function.py
@@ -24,14 +24,6 @@ def lambda_handler(event, context):
           "anthropic_version": "bedrock-2023-05-31"
         }
      )
-     
-     # The actual call to retrieve an answer from the model
-    # response = bedrock_runtime.invoke_model(
-    #    body=body, 
-    #    modelId="ai21.j2-ultra-v1", 
-    #    accept='application/json', 
-    #    contentType='application/json'
-    #  )
 
     response = bedrock_runtime.invoke_model(
        body=body, 
@@ -53,29 +45,3 @@ def lambda_handler(event, context):
        },
          'body': json.dumps({ "Answer": answer })
        }
-
-# import os
-# import os.path
-# import sys
- 
-# envLambdaTaskRoot = os.environ["LAMBDA_TASK_ROOT"]
-# print("LAMBDA_TASK_ROOT env var:"+os.environ["LAMBDA_TASK_ROOT"])
-# print("sys.path:"+str(sys.path))
- 
-# sys.path.insert(0,envLambdaTaskRoot+"/NewBotoVersion")
-# print("sys.path:"+str(sys.path))
-# import botocore
-# import boto3
- 
-# print("boto3 version:"+boto3.__version__)
-# print("botocore version:"+botocore.__version__)
-
- 
-# def lambda_handler(event,context):
-#     b = 'Why is Pluto no longer a planet?'.encode('utf-8')
-#     client = boto3.client('bedrock-runtime')
-#     response = client.invoke_model(
-#     body=b,
-#     modelId='anthropic.claude-v2:1'
-#     )
-#     print(event)
